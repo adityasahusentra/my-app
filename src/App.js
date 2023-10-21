58cd89dff8e7790fa11c9a07511b12afbc139974
Aditya Sahu <aditya.sahu@sentra.world>
2023-10-21 22:51:45 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomeScreen from './components/home-screen/home';
+import QuestionScreen from './components/question/question';
+import ResultScreen from './components/result/result';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" exact element={<HomeScreen/>} />
+          <Route path="/question" element={<QuestionScreen/>} />
+          <Route path="/result" element={<ResultScreen/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
